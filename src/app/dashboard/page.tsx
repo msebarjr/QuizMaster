e1a545fdacf6005c225c138f3e9eb91a958945ef
msebarjr <msebarjr@gmail.com>
2023-10-13 22:59:35 -0400

    Added Quiz and History Cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { getAuthSession } from '@/lib/nextauth'
+import { redirect } from 'next/navigation'
+
+// Custom Components
+import { QuizMeCard, HistoryCard } from '@/components/Cards'
 
 type Props = {}
 
-const Dashboard = (props: Props) => {
+const Dashboard = async (props: Props) => {
+  const session = await getAuthSession()
+
+  if (!session?.user) return redirect('/')
+
   return (
-    <div>Dashboard</div>
+    <main className='p-8 mx-auto max-w-7xl'>
+      <div className="flex items-center">
+        <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        <QuizMeCard />
+        <HistoryCard />
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7"></div>
+    </main>
   )
 }
 
---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,2 @@
+export { default as QuizMeCard } from './QuizMeCard'
+export { default as HistoryCard } from './HistoryCard'

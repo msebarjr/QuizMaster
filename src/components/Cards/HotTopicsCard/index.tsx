5acda96ccd80d3ed64ef9d7759b1e4c255062020
msebarjr <msebarjr@gmail.com>
2023-10-13 23:34:06 -0400

    Added Word Cloud integration

--- src/components/Cards/HotTopicsCard/index.tsx
+++ src/components/Cards/HotTopicsCard/index.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 // shadcn Components
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
+// Custom Components
+import WordCloud from '@/components/WordCloud'
+
 type Props = {}
 
 const HotTopicsCard = (props: Props) => {
@@ -12,7 +15,7 @@ const HotTopicsCard = (props: Props) => {
         <CardTitle className='text-2xl font-bold'>Hot Topics</CardTitle>
         <CardDescription>Click on a topic to start a quiz!</CardDescription>
       </CardHeader>
-      <CardContent className='pl-2'>Word Cloud</CardContent>
+      <CardContent className='pl-2'><WordCloud /></CardContent>
     </Card>
   )
 }
